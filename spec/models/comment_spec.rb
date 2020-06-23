79c9f205663b9d036da68218c40da7d7303ea3ef
Ferm0494 <ferm0494@gmail.com>
2020-06-23 16:23:11 -0600

    Adding features

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+RSpec.describe Comment,type: :model do
+    context "Validations for comment" do
+    let(:user) { User.new(email: 'testing@testing.com', name: 'testing', password: 'foobar') }
+    let(:post) { Post.new(content: 'Content for post') }
+    let(:comment) {Comment.new(content: "Nice post!")}
+    
+    it "Should be a valid comment if content not empty "do
+        user.save
+        post.save
+        comment.user = user
+        comment.post = post
+        expect(comment).to be_valid
+    end
+
+    it "Should not be a valid comment if content not empty "do
+        user.save
+        post.save
+        comment.user = user
+        comment.post = post
+        comment.content = ""
+        expect(comment).not_to be_valid
+    end
+
+    it "Should not be valid if theres no post "do
+        user.save
+        post.save
+        comment.user = user
+        comment.post = nil
+        expect(comment).not_to be_valid
+    end
+
+    it "Should not be valid if theres no user "do
+        user.save
+        post.save
+        comment.user = nil
+        comment.post = post
+        expect(comment).not_to be_valid
+    end
+ end
+ context "Assocs for Comments" do
+     it {should belong_to(:user)}
+     it {should belong_to(:post)}
+ end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    context "Validation for user" do
-        let(:user){User.new(email: "testing@testing.com", name:"testing",password: "foobar")}
+  context 'Validation for user' do
+    let(:user) { User.new(email: 'testing@testing.com', name: 'testing', password: 'foobar') }
 
-        it "Should be valid if username has email/name/password field completed password 6+" do
-            expect(user).to  be_valid
-
-        end
-        it "Should be invalid if password is less than 6 chars" do
-            user.password = "fooba"
-            expect(user).not_to be_valid
-        
-        end
-
-        it "Should be invalid if email is blank" do
-            user.email = " "
-            expect(user).not_to be_valid
-        
-        end
-
-        it "Should be invalid if name is blank" do
-            user.name = " "
-            expect(user).not_to be_valid
-        
-        end
-
-        it "Should be invalid if name is 20+ is blank" do
-            user.name = "f" * 21
-            expect(user).not_to be_valid
-        
-        end
-
-
+    it 'Should be valid if username has email/name/password field completed password 6+' do
+      expect(user).to be_valid
+    end
+    it 'Should be invalid if password is less than 6 chars' do
+      user.password = 'fooba'
+      expect(user).not_to be_valid
     end
 
-    context "Assocs for User" do
-        it {should have_many(:posts)}
-        it {should have_many(:comments)}
-        it {should have_many(:likes)}
-        it {should have_many(:friendships).with_foreign_key('user_id other_user_id')}
+    it 'Should be invalid if email is blank' do
+      user.email = ' '
+      expect(user).not_to be_valid
     end
+
+    it 'Should be invalid if name is blank' do
+      user.name = ' '
+      expect(user).not_to be_valid
+    end
+
+    it 'Should be invalid if name is 20+ is blank' do
+      user.name = 'f' * 21
+      expect(user).not_to be_valid
+    end
+  end
+
+  context 'Assocs for User' do
+    it { should have_many(:posts) }
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
+    it { should have_many(:friendships).with_foreign_key('user_id other_user_id') }
+  end
 end
